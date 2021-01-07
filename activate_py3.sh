53d94b7b3a18e3d21a06d8715c029f295face64f
David Ruhe <david.ruhe@gmail.com>
2021-01-07 13:32:08 +0100

    start implementing gfft

--- activate_py3.sh
+++ activate_py3.sh
@@ -4,13 +4,18 @@ ENV_DIR='.venv_py3'
 
 if [[ ! -d $ENV_DIR ]]
 then
-    echo "Installing required packages."
     python3 -m venv $ENV_DIR
     source $ENV_DIR/bin/activate
-    pip install -r requirements.txt -f https://download.pytorch.org/whl/torch_stable.html
+    # pip install -r requirements_py3.txt -f https://download.pytorch.org/whl/torch_stable.html
 else
     echo "Activating environment."
     source $ENV_DIR/bin/activate
 fi
 
+# DIFF=$(comm -23 <(sort requirements_py3.txt) <(pip freeze | sort| sed s/=.*//))
+# pip install $DIFF -f https://download.pytorch.org/whl/torch_stable.html
+
+pip -q install -r requirements_py3.txt -f https://download.pytorch.org/whl/torch_stable.html
+
 export PYTHONPATH='.'
+export JUPYTER_PATH='.'
